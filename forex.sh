567f164886a24d77d021db70c642011438bbd10f
DhyanShah22 <dhyanshah229@gmail.com>
2024-01-01 19:09:46 +0530

    New scripts added

---
+++ forex.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Simple shell script with for loop
+
+echo "Counting from 1 to 5:"
+
+# Using a for loop to iterate from 1 to 5
+for i in {1..5}; do
+    echo $i
+done
+
+echo "Loop completed!"
+
