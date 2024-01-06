f60dd4d5bd1e42eb6b54d634aa36f42f8a01e37f
DhyanShah22 <dhyanshah229@gmail.com>
2024-01-06 12:55:52 +0530

    Basic Operation Shell Script

---
+++ colourArr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+# Araay 1
+
+Fruits[0]="Apple"
+Fruits[1]="Oranges"
+Fruits[2]="Grapes"
+
+echo "First Element: ${Fruits[0]}"
+echo "Third Element: ${Fruits[2]}"
+
+echo "All the Elements: ${Fruits[@]}"
+
+
