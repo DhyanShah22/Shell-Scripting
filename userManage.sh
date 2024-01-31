981ba1b13376b0205fb2e1c04dac5dd8abe50283
DhyanShah22 <dhyanshah229@gmail.com>
2024-01-31 15:12:10 +0530

    Added User Management System

---
+++ userManage.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "User Management System"
+echo "______________________"
+
+echo "a) Add User"
+echo "b) Remove User"
+echo "c) List User"
+
+read -p "Select an option: " choice
+
+case $choice in
+	a)
+		read -p "Enter Username: " new_user
+		sudo useradd $new_user
+		echo "User $new_user added successfully!!"
+		;;
+	b)
+		read -p "Enter Username: " remove_user
+		sudo userdel -r $remove_user
+		echo "User $remove_user removed successfully!!"
+		;;
+	c)
+		echo "List of users: "
+		cut -d: -f1 /etc/passwd
+		;;
+	*)
+		echo "Invalid Option"
+		;;
+esac
