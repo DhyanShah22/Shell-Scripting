2a8ab9a7d878a1325ae04dab7039984272955e4a
DhyanShah22 <dhyanshah229@gmail.com>
2024-02-07 13:07:33 +0530

    IA prep 8.0

--- IA/leap.sh
+++ IA/leap.sh
@@ -2,11 +2,11 @@
 
 read -p "Enter the year: " year
 
-if [ $(($year % 4)) -eq 0 ]
+if [ $((year % 4)) -eq 0 ]
 then 
-	if [ $(($year % 100)) -eq 0 ]
+	if [ $((year % 100)) -eq 0 ]
 	then
-		if [ $(($year % 400)) -eq 0 ]
+		if [ $((year % 400)) -eq 0 ]
 		then
 			echo "This is a leap year."
 		else
