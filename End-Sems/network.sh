b6b8acbb24cf6c4f7bbb26ee48f56482d484cf59
DhyanShah22 <dhyanshah229@gmail.com>
2024-04-29 21:26:19 +0530

    End-Sems 6

---
+++ End-Sems/network.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Network Information System"
+echo "__________________________"
+
+echo "Hostname : $(hostname)"
+echo "IP Address : $(hostname -I)"
+echo "Network INterface : $(ifconfig)"
